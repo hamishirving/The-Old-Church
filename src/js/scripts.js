13707b06caef34b11c09f7fcedbaa8a0dfcd52f4
Hamish Irving <hamishirving@Hamishs-MacBook-Pro.local>
2015-09-18 19:56:48 +0100

    Added Dining page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -97,4 +97,44 @@ $( document ).ready(function() {
         var attachFastClick = Origami.fastclick;
         attachFastClick(document.body);
     });
+
+    // ----
+    // Fade in element on scroll
+    // ----
+
+    fadeIn = $(".fadein").fadeTo(0, 0);
+
+    $(window).scroll(function(d,h) {
+        fadeIn.each(function(i) {
+            a = $(this).offset().top + $(this).height();
+            b = $(window).scrollTop() + $(window).height();
+            if (a < b) $(this).addClass("animated fadeInDown");
+        });
+    });
+
+    // ----
+    // Tabby - http://gomakethings.com
+    // ----
+
+    (function($) {
+        $(function () {
+            $('.tabs a, .tabs button').click(function(e) {
+                e.preventDefault(); // Prevent default link behavior.
+                var toggle = $(this);
+                var tabID = toggle.attr('data-target'); // Get the ID of tab
+
+                toggle.addClass('active').parent().addClass('active'); // Add the .active class to the link and it's parent li (if one exists).
+                toggle.siblings().removeClass('active'); // Remove the .active class from sibling tab navigation elements.
+                toggle.parent('li').siblings().removeClass('active').children().removeClass('active'); // Remove the .active class from sibling li elements and their links.
+                $(tabID).addClass('active'); // Add the .active class to the div with the tab content.
+                $(tabID).siblings().removeClass('active'); // Remove the .active class from other tab content divs.
+            });
+        });
+    })(jQuery);
+
+    (function($) {
+        $(function () {
+            $('body').addClass('js'); // On page load, add the .js class to the <body> element.
+        });
+    })(jQuery);
 });

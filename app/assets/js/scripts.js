6c7f407c2781c20d7d028326a5d44d846cc506cd
Hamish Irving <hamishirving@Hamishs-MacBook-Pro.local>
2015-11-07 09:12:34 +0000

    Test smoothstate js

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -141,4 +141,46 @@ $( document ).ready(function() {
         e.preventDefault();
     });
 
+    // Wait for window load
+    $(window).load(function() {
+        // Animate loader off screen
+        $(".loading").fadeOut("slow");
+    });
+
+    // ----
+    // Smoothstate.js
+    // ----
+
+    $(function(){
+      'use strict';
+      var options = {
+        prefetch: true,
+        cacheLength: 2,
+        anchors: 'nav ul li a',
+        blacklist: '.no-smoothState',
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+
+            // Inject the new content
+            $container.html($newContent);
+
+          }
+        }
+      },
+      smoothState = $('#main').smoothState(options).data('smoothState');
+    });
+
 });
